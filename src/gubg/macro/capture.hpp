2b7d32a329d5038c8ca132d3cf391425246d350b
Geert Fannes <geert.fannes@gmail.com>
2024-04-03 08:44:44 +0200

    Indentation

--- src/gubg/macro/capture.hpp
+++ src/gubg/macro/capture.hpp
@@ -4,11 +4,11 @@
 #include <gubg/macro/variadic.h>
 
 #ifdef C
-#error C already defined
+    #error C already defined
 #endif
 
 #define C_1(var) "(" #var ":" << var << ")"
 #define C_2(var, type) "(" #var ":" << type(var) << ")"
-#define C(...) GUBG_GET_ARG_3((__VA_ARGS__, C_2,C_1))(__VA_ARGS__)
+#define C(...) GUBG_GET_ARG_3((__VA_ARGS__, C_2, C_1))(__VA_ARGS__)
 
 #endif
